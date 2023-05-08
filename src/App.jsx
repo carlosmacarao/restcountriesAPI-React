05563be7236e9d37863941070b90c6415f1dcd2a
Carlos Macarao <82442020+carlosmacarao@users.noreply.github.com>
2023-05-08 10:59:04 +0200

    Rest Countries API

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import viteLogo from '/vite.svg'
 import './App.css'
+import AllCountries from './assets/AllCountries/AllCountries';
+import CountryInfo from './assets/CountryInfo/CountryInfo';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="header">
+        <div className="container">
+          <h5>Where in the world</h5>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="container">
+        <BrowserRouter> 
+          <Routes>
+            <Route path="/" element={<AllCountries />} />
+            <Route path="/country/:countryName" element={<CountryInfo />} />
+          </Routes>
+        </BrowserRouter>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
